6045dff9d911d516c662024fa81f837af91d4354
doriancacheleux <doriancacheleux@localhost>
2025-03-28 12:40:28 +0100

    commit initial

---
+++ src/main/java/com/mycompany/projet_info_s2/Projet_Info_S2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_info_s2;
+
+/**
+ *
+ * @author doriancacheleux
+ */
+public class Projet_Info_S2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
