6580d3c4f831ac6d6ba1bc2edaee5241856ff55a
doriancacheleux <doriancacheleux@localhost>
2025-04-07 12:09:20 +0200

    création enum pour gérer de manière les différents états d'une machine

---
+++ src/main/java/com/mycompany/projet_info_s2/EtatMachine.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.projet_info_s2;
+
+/**
+ *
+ * @author doriancacheleux
+ */
+public enum EtatMachine {
+    OPERATIONNEL,
+    EN_PANNE,
+    EN_MAINTENANCE,
+    OCCUPEE,
+    DISPONIBLE
+}
